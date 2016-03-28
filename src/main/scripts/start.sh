263781017adef797338c87b4865a8ee6fa59c522
turu <piotr.turu.turek@gmail.com>
2016-03-28 20:08:21 +0200

    extracted gc, jmx and heap params to pom, fixed a small bug with OOM heapdump and decreased default size of heap to value suitable for non-bigdata applications

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -8,19 +8,23 @@ fi
 cmd_args=$@
 env=${1:-default}
 
-jvm_xms=${2:-2g}
-jvm_xmx=${3:-2g}
-new_size=128m
+jvm_xms=${2:-@jvmArgs.dev.xms@}
+jvm_xmx=${3:-@jvmArgs.dev.xmx@}
+new_size=@jvmArgs.dev.newSize@
+gc_args="@jvmArgs.dev.gc@"
+jmx_args="@jvmArgs.dev.jmx@"
 if [ "$env" = "prod" ]; then
-    jvm_xms=${2:-15g}
-    jvm_xmx=${3:-15g}
-    new_size=3g
+    jvm_xms=${2:-@jvmArgs.prod.xms@}
+    jvm_xmx=${3:-@jvmArgs.prod.xmx@}
+    new_size=@jvmArgs.prod.newSize@
+    gc_args="@jvmArgs.prod.gc@"
+    jmx_args="@jvmArgs.prod.jmx@"
 fi
 
 hostname=`hostname -I | cut -d' ' -f1`
 
-jvm_args="-Xms${jvm_xms} -Xmx${jvm_xmx} -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewSize=${new_size} -XX:+CMSParallelRemarkEnabled -Djava.net.preferIPv4Stack=true -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=${hostname} -verbosegc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps
--XX:-HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError=./restart.sh"
+jvm_args="-Xms${jvm_xms} -Xmx${jvm_xmx} -XX:NewSize=${new_size} -Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=${hostname} \
+-XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError=./restart.sh ${gc_args} ${jmx_args}"
 shift 3
 extra_args=$@
 
