c07c376a24fd275bcc5e8d69130df656c200396c
turu <piotr.turu.turek@gmail.com>
2016-04-03 02:51:14 +0200

    redirect console logger to dev/null on prod

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -35,11 +35,13 @@ jvm_xms=${3:-@jvmArgs.dev.xms@}
 jvm_xmx=${4:-@jvmArgs.dev.xmx@}
 gc_args="@jvmArgs.dev.gc@"
 jmx_args="@jvmArgs.dev.jmx@"
+outfile="std.out"
 if [ "$env" = "prod" ]; then
     jvm_xms=${3:-@jvmArgs.prod.xms@}
     jvm_xmx=${4:-@jvmArgs.prod.xmx@}
     gc_args="@jvmArgs.prod.gc@"
     jmx_args="@jvmArgs.prod.jmx@"
+    outfile="/dev/null"
 fi
 gc_args="${gc_args} -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError=./restart.sh"
 jmx_args="${jmx_args} -Djava.rmi.server.hostname=${hostname} -Dcom.sun.management.jmxremote.port=${jmx_port}"
@@ -50,7 +52,7 @@ extra_args=$@
 
 # Start JVM
 set -o xtrace
-nohup java ${jvm_args} -jar @name@-@project.version@.jar --spring.profiles.active=${env} ${extra_args} > std.out &
+nohup java ${jvm_args} -jar @name@-@project.version@.jar --spring.profiles.active=${env} ${extra_args} > ${outfile} 2>&1 &
 set +o xtrace
 
 pid=$!
