5a69aead9ba556d6db1dce07cb95da5aec878ab9
turu <piotr.turu.turek@gmail.com>
2016-04-03 02:45:59 +0200

    * added comments to start.sh * dynamically assign jmx port * allow to supply custom app server port * removed new size param * changed java version to 1.8

--- src/main/scripts/start.sh
+++ src/main/scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script starts a microservice-boot app.
+# Usage: ./start.sh [<env={default,prod}>] [<app_port:@app.port@>] [<xms:{@jvmArgs.dev.xms@,@jvmArgs.prod.xms@}>] [<xmx:{@jvmArgs.dev.xmx@,@jvmArgs.prod.xmx@}>] [<extra_args>]
 
 if [ -f service.pid ]; then
     echo "Service already running!"
@@ -8,32 +10,53 @@ fi
 cmd_args=$@
 env=${1:-default}
 
-jvm_xms=${2:-@jvmArgs.dev.xms@}
-jvm_xmx=${3:-@jvmArgs.dev.xmx@}
-new_size=@jvmArgs.dev.newSize@
+app_port=${2:-@app.port@}
+
+# Find hostname and available JMX port
+hostname=`hostname -I | cut -d' ' -f1`
+
+jmx_port=1099
+is_free=$(netstat -tapln | grep ${jmx_port})
+tries=0
+
+while [[ -n "${is_free}" || "${jmx_port}" = "${app_port}" && tries < 1000 ]]; do
+    jmx_port=$[jmx_port+1]
+    tries=$[tries+1]
+    is_free=$(netstat -tapln | grep ${jmx_port})
+done
+
+if [[ -n "${is_free}" || "${jmx_port}" == "${app_port}" ]]; then
+    echo "Failed to find available port for JMX endpoint"
+    exit 2
+fi
+
+# Compose JVM (GC, JMX) parameters
+jvm_xms=${3:-@jvmArgs.dev.xms@}
+jvm_xmx=${4:-@jvmArgs.dev.xmx@}
 gc_args="@jvmArgs.dev.gc@"
 jmx_args="@jvmArgs.dev.jmx@"
 if [ "$env" = "prod" ]; then
-    jvm_xms=${2:-@jvmArgs.prod.xms@}
-    jvm_xmx=${3:-@jvmArgs.prod.xmx@}
-    new_size=@jvmArgs.prod.newSize@
+    jvm_xms=${3:-@jvmArgs.prod.xms@}
+    jvm_xmx=${4:-@jvmArgs.prod.xmx@}
     gc_args="@jvmArgs.prod.gc@"
     jmx_args="@jvmArgs.prod.jmx@"
 fi
+gc_args="${gc_args} -XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError=./restart.sh"
+jmx_args="${jmx_args} -Djava.rmi.server.hostname=${hostname} -Dcom.sun.management.jmxremote.port=${jmx_port}"
+jvm_args="-Xms${jvm_xms} -Xmx${jvm_xmx} -Dserver.port=${app_port} -Djava.net.preferIPv4Stack=true ${gc_args} ${jmx_args}"
 
-hostname=`hostname -I | cut -d' ' -f1`
-
-jvm_args="-Xms${jvm_xms} -Xmx${jvm_xmx} -XX:NewSize=${new_size} -Djava.net.preferIPv4Stack=true -Djava.rmi.server.hostname=${hostname} \
--XX:+HeapDumpOnOutOfMemoryError -XX:OnOutOfMemoryError=./restart.sh ${gc_args} ${jmx_args}"
-shift 3
+shift 4
 extra_args=$@
 
+# Start JVM
 set -o xtrace
 nohup java ${jvm_args} -jar @name@-@project.version@.jar --spring.profiles.active=${env} ${extra_args} > std.out &
 set +o xtrace
 
 pid=$!
 sleep 25
+
+# Verify that service running
 is_running=`netstat -pan | grep "$pid"`
 if [ ! -z "$is_running" ]; then
     echo "Service successfully started in env: $env. PID: $pid"
